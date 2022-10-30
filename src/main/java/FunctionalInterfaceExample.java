8c9de597ced1487f321bd5b0f3daf520eb223a21
youngbin-yun <youngbin@koreaspacedata.com>
2022-10-30 23:28:51 +0900

    feature: use Consumer

--- src/main/java/FunctionalInterfaceExample.java
+++ src/main/java/FunctionalInterfaceExample.java
@@ -1,7 +1,11 @@
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class FunctionalInterfaceExample {
     public static void main(String[] args) {
+        /**
+         * Function
+         */
         final Function<String, Integer> toInt = s -> Integer.parseInt(s);
 
         final Integer number = toInt.apply("100");
@@ -14,5 +18,14 @@ public class FunctionalInterfaceExample {
         // 직접 만들고 싶다면
         final Function<Integer, Integer> identity2 = t -> t;
         System.out.println(identity2.apply(998));
+
+        /**
+         * Consumer
+         */
+        final Consumer<String> print = value -> System.out.println(value);
+        final Consumer<String> greetings = value -> System.out.println("hello " + value);
+
+        print.accept("hello");
+        greetings.accept("World");
     }
 }
