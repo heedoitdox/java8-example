6d817f2c512332d599cf44aafac0b45fcf970ce8
youngbin-yun <youngbin@koreaspacedata.com>
2022-10-30 23:08:12 +0900

    feature: FunctionalInterfaceExample.java

---
+++ src/main/java/FunctionalInterfaceExample.java
@@ -0,0 +1,18 @@
+import java.util.function.Function;
+
+public class FunctionalInterfaceExample {
+    public static void main(String[] args) {
+        final Function<String, Integer> toInt = s -> Integer.parseInt(s);
+
+        final Integer number = toInt.apply("100");
+        System.out.println(number);
+
+        // Function 에 있는 identity 를 그대로 사용
+        final Function<Integer, Integer> identity = Function.identity();
+        System.out.println(identity.apply(999));
+
+        // 직접 만들고 싶다면
+        final Function<Integer, Integer> identity2 = t -> t;
+        System.out.println(identity2.apply(998));
+    }
+}
