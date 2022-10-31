84c4c45b0acfa1ce250c28044a7fffa6ef595b67
youngbin-yun <youngbin@koreaspacedata.com>
2022-10-31 22:58:00 +0900

    feature: 06. make my Functional interface

--- src/main/java/FunctionalInterfaceExample.java
+++ src/main/java/FunctionalInterfaceExample.java
@@ -8,7 +8,7 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class FunctionalInterfaceExample {
-    public static void main(String[] args) {
+    public static void runFunctional(String[] args) {
         /**
          * Function
          */
@@ -101,4 +101,18 @@ public class FunctionalInterfaceExample {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        println(1L, "heedoitdox", "heedoitdox@gmail.com",
+                (id, name, email) -> "User info: ID = " + id + ", name = " + name + " , email = " + email );
+    }
+
+    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function){
+        System.out.println(function.apply(t1, t2, t3));
+    }
+}
+
+@FunctionalInterface
+interface Function3<T1, T2, T3, R> {
+    R apply(T1 t1, T2 t2, T3 t3);
 }
