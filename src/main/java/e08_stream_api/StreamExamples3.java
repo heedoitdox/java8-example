85d5bcc19adad68a277fb15981e48c20b00369d4
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-30 23:35:40 +0900

    chore(08): intermediate & terminal method (toSet)

--- src/main/java/e08_stream_api/StreamExamples3.java
+++ src/main/java/e08_stream_api/StreamExamples3.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class StreamExamples3 {
     public static void main(String[] args) {
         System.out.println("collection(toList()): " +
-                Stream.of(1, 2, 3, 4, 5)
+                Stream.of(1, 3, 3, 5, 5)
                         .filter(i -> i > 2)
                         .map(i -> i * 2)
                         .map(i -> "#" + i)
@@ -18,5 +19,14 @@ public class StreamExamples3 {
 
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         final Stream<Integer> integerStream = Stream.of(1,2,3,4,5);
+
+        // set 으로 받기
+        System.out.println("collection(toList()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(toSet())
+        );
     }
 }
